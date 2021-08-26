18b55648f7cdf0d4f9979c58a22a5fccc6e667b5
Thomas O'Neill <tom@bearjam.dev>
2021-08-26 13:05:21 +0100

    tw screens to stitches media

--- stitches.config.ts
+++ stitches.config.ts
@@ -1,11 +1,14 @@
 import { createCss, StitchesCss } from "@stitches/react";
+import { map } from "fp-ts/lib/ReadonlyRecord"
+import tailwindConfig from "./tailwind.config"
 export type { StitchesVariants } from "@stitches/react";
 
 export const stitches = createCss({
-  prefix: '',
+  prefix: "",
   theme: {},
   themeMap: {},
   utils: {},
+  media: map((px) => `(min-width: ${px})`)(tailwindConfig.theme.screens),
 })
 
 export type CSS = StitchesCss<typeof stitches>;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
 module.exports = {
   theme: {
+    screens: {
+      ...defaultTheme.screens,
+    },
     extend: {
       colors: {
         electric: '#db00ff',
         ribbon: '#0047ff',
       },
-    },
+    }
   },
   plugins: [],
 }
